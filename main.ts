ce1e06bce886b9233d5763a3b8bca82ea64ec201
Jolieknight <118588607+Jolieknight@users.noreply.github.com>
2022-11-20 18:42:43 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+let set_hand_to = 0
+input.onGesture(Gesture.Shake, function () {
+    let hand = ""
+    set_hand_to = randint(1, 3)
+    if (hand == "1") {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (hand == "2") {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
